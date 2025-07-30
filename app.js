7a4e397f545a7de26dd6bf0a0c95bdcfd7b04df4
Angelo Nascimento <angeloinacio321@gmail.com>
2025-07-30 12:59:09 -0300

    commit inserção de botão e função novo sorteio

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
-//  Aqui você deverá desenvolver a lógica para resolver o problema.
-//1- Criar array de nomes*
-//2- implementar função para adicionar nomes a lista de amigos*
-//3- implmentar função para atualizar lista de amigos*
-//4- implementar função para sortear os amigos.*
+/*modifica a função add para ver se contem numeros iguais, ou se ta em branco*/
+
+//extra adcionar botão novo jogo limpa campos e listas 
 let nomesAmigos = [];//lista com os nomes dos amigos
 
 
 function adicionaLista(){
     let novosNome = document.getElementById('amigo').value.trim().trimEnd();//trim() e trimEnd() removem os espaços em branco no inicio e no fim respectivamente         
-    if (novosNome != ""){//verifica se o campo input esta vazio
+    if (novosNome !== ""){//verifica se o campo input esta vazio
        nomesAmigos.push(novosNome);//se o campo estiver preenchido adiciona o nome a lista
-       //console.log(nomesAmigos);
        listaAmigos();
        limpaInput();
        
+       
         
     }else {//se estiver com o campo vazio manda um alert para preencher o campo.
-        alert('digite um nome valido!');
+        alert('Por favor, insira um nome valido!');
     }
    
 }
+function limpaLista(){
+    let nomesListados = document.getElementById('listaAmigos');
+    nomesListados.innerHTML = '';
+}
+
 function limpaInput(){//limpa o campo ao adicionar o nome na lista
     novosNome = document.querySelector('input');
     novosNome.value = '';
 }
+
 function listaAmigos(){//exibe o nome dos amigos adicionados a lista
     let nomesListados = document.getElementById('listaAmigos');
     nomesListados.innerHTML = '';
@@ -32,21 +35,27 @@ function listaAmigos(){//exibe o nome dos amigos adicionados a lista
         let itemNome = document.createElement('li');
         itemNome.textContent = nomesAmigos[i];
         nomesListados.appendChild(itemNome);
-        //console.log(nomesListados);
     }
     
 }
 
-function sorteio(){
+function sorteio(){//realiza o soretio dos nomes
     if (nomesAmigos.length < 2){
         alert('participantes insuficientes!')
     }else{
-        const sorteioDoNome = Math.floor(Math.random() * nomesAmigos.length);
+        const sorteioDoNome = Math.floor(Math.random() * nomesAmigos.length);//faz o sorteio do nome
         const nomesorte = nomesAmigos[sorteioDoNome];
-        document.getElementById('resultado').innerText = "o Amigo sorteado é: " + nomesorte;
+        limpaLista();
+        document.getElementById('resultado').innerText = 'o Amigo sorteado é: ' + nomesorte;
+        document.getElementById('reiniciar').removeAttribute('disabled');
       
     }
+}
 
-    
-
+//função para botão de novo sorteio.
+function novoSorteio(){//alterações no html botao novo, e no js função novoSorteio
+    nomesAmigos = [];
+    document.getElementById('resultado').innerText = '';
+    document.getElementById('reiniciar').disabled = true;
+     
 }
